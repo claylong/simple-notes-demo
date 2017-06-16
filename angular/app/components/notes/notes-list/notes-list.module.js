7d3361efcee1c9a493885f6e0b880178a0e303f3
Clay Long <clay.long@gmail.com>
2017-06-15 23:32:11 -0400

    Implement a "masonry style" grid for Notes

--- angular/app/components/notes/notes-list/notes-list.module.js
+++ angular/app/components/notes/notes-list/notes-list.module.js
@@ -1,10 +1,13 @@
 import angular from 'angular';
 import { NotesListComponent } from './notes-list.component';
 import { NoteModule } from '../note/note.module';
+import 'angular-deckgrid/angular-deckgrid.js';
+import './notes-list.css';
 
 export const NotesListModule = angular
   .module('notes.list', [
-    NoteModule
+    NoteModule,
+    'akoenig.deckgrid'
   ])
   .component('notesList', NotesListComponent)
   .name;
